3c7906311b88b447643e3e87a69399a10b55133b
mannsn <mannsn1@verizon.net>
2024-10-01 21:28:01 -0400

    lesson1

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const country = "USA";
 console.log("My first name is: ", firstName);
 console.log("My last name is: ", lastName);
 console.log("I was born in the country: ", country);
+console.log ("hi");
